7504c770caab5608ada43720ac1f8bfa5d057e29
Dragos Serban <dragos.serban@philips.com>
2020-08-28 12:25:01 +0200

    AXQ0jPRewHPalJICIyLG Fix <Remove this unused "bookNumOfPages" local variable.>

--- src/main/java/com/philips/libraryapp/LibraryService.java
+++ src/main/java/com/philips/libraryapp/LibraryService.java
@@ -13,7 +13,6 @@ public class LibraryService {
 	public static final ActionLogger actionLogger = new ActionLogger();
 	
 	public String addBook(String bookName) {
-		int bookNumOfPages;
 		boolean bookIsNonfiction, languageIsEnglish;
 		String isbn  = "";
 
